df729f4e2894fc43ebfe9259dbefd14aa34d508f
jeheskielSunloy77 <jeheskielventiokysunloy@gmail.com>
2024-02-01 16:43:55 +0700

    chore: restyled some components and remove unused components

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -1,10 +1,19 @@
 @props(['on'])
 
-<div x-data="{ shown: false, timeout: null }"
-     x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000); })"
-     x-show.transition.out.opacity.duration.1500ms="shown"
-     x-transition:leave.opacity.duration.1500ms
-     style="display: none;"
-    {{ $attributes->merge(['class' => 'text-sm text-gray-600 dark:text-gray-400']) }}>
-    {{ $slot->isEmpty() ? 'Saved.' : $slot }}
+<div x-data="{ shown: false, timeout: null }" x-init="@this.on('{{ $on }}', () => {
+    clearTimeout(timeout);
+    shown = true;
+    timeout = setTimeout(() => { shown = false }, 2000);
+})" x-show.transition.out.opacity.duration.1500ms="shown"
+    x-transition:leave.opacity.duration.1500ms style="display: none;"
+    {{ $attributes->merge(['class' => 'flex items-center p-4 border-t-4 border-black bg-gray-50 dark:bg-gray-800 dark:border-gray-600']) }}
+    role="alert">
+    <svg class="flex-shrink-0 w-4 h-4 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor" viewBox="0 0 20 20">
+        <path
+            d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+    </svg>
+    <div class="ms-3 text-sm font-medium text-gray-800 dark:text-gray-300">
+        {{ $slot }}
+    </div>
 </div>
